ca91ed725953958789a5d76ab5eb967b3d13baea
Rahul Kushaha <gamecreator762@gmail.com>
2020-09-29 14:41:17 +0530

    Code optimized

--- MainProjectDjango/minorproject1/backend/views.py
+++ MainProjectDjango/minorproject1/backend/views.py
@@ -26,7 +26,7 @@ import time
 
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture(0)
+        self.video = cv2.VideoCapture(0,cv2.CAP_DSHOW)
     def __del__(self):
         self.video.release()
 
